d6951b77741a569cc5058ffe69f9a2f27d432a25
Jonathan Tromp <jonatromp45@gmail.com>
2024-12-11 18:34:22 +0000

    user page editing

--- app/screens/UserPageScreen.js
+++ app/screens/UserPageScreen.js
@@ -1,10 +1,24 @@
 import React from "react";
-import { View, StyleSheet, Text, SafeAreaView } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  Alert,
+} from "react-native";
 
 function UserPageScreen() {
   return (
     <SafeAreaView>
       <Text>Profile Page</Text>
+      <Image></Image>
+      <Text>Username</Text>
+      <Button
+        onPress={() => Alert.alert("edit account details")}
+        title="Edit Account"
+      ></Button>
     </SafeAreaView>
   );
 }
