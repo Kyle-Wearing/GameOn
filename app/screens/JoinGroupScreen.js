f85d92da621282de3cf93919c33867ea5e0a22f2
Jonathan Tromp <jonatromp45@gmail.com>
2024-12-11 16:25:39 +0000

    edited navigation

--- app/screens/JoinGroupScreen.js
+++ app/screens/JoinGroupScreen.js
@@ -2,7 +2,11 @@ import React from "react";
 import { Text, View, SafeAreaView, StyleSheet, TextInput } from "react-native";
 
 function JoinGroupScreen() {
-  return <SafeAreaView></SafeAreaView>;
+  return (
+    <SafeAreaView>
+      <TextInput value={"Enter Group Code Here"} />
+    </SafeAreaView>
+  );
 }
 
 export default JoinGroupScreen;
