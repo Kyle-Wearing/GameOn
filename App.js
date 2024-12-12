2054f4ed66e564b41e7a1385083101136e49bb18
Jonathan Tromp <jonatromp45@gmail.com>
2024-12-12 19:06:01 +0000

    added navigation bar functionality in app.js

--- App.js
+++ App.js
@@ -3,9 +3,79 @@ import CreateAccountScreen from "./app/screens/CreateAccountScreen";
 import HomePageScreen from "./app/screens/HomePageScreen";
 import UserPageScreen from "./app/screens/UserPageScreen";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View, PlatformPressable } from "react-native";
+import {
+  NavigationContainer,
+  useLinkBuilder,
+  useTheme,
+} from "@react-navigation/native";
+// import { PlatformPressable } from "react-native/elements";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+function MyTabBar({ state, descriptors, navigation }) {
+  const { colors } = useTheme();
+  const { buildHref } = useLinkBuilder();
+
+  return (
+    <View style={{ flexDirection: "row" }}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: "tabLongPress",
+            target: route.key,
+          });
+        };
+
+        return (
+          <PlatformPressable
+            href={buildHref(route.name, route.params)}
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarButtonTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{ flex: 1 }}
+          >
+            <Text style={{ color: isFocused ? colors.primary : colors.text }}>
+              {label}
+            </Text>
+          </PlatformPressable>
+        );
+      })}
+    </View>
+  );
+}
+
+const MyTabs = createBottomTabNavigator({
+  tabBar: (props) => <MyTabBar {...props} />,
+  screens: {
+    Home: HomePageScreen,
+    Profile: UserPageScreen,
+  },
+});
 
 const Stack = createNativeStackNavigator();
 
--- app/screens/HomePageScreen.js
+++ app/screens/HomePageScreen.js
@@ -2,18 +2,9 @@ import React from "react";
 // import UserPageScreen from "./app/screens/UserPageScreen";
 // import JoinGroupScreen from "./app/screens/JoinGroupScreen";
 import { View, StyleSheet, Text, SafeAreaView } from "react-native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
-// const MyTabs = createBottomTabNavigator({
-//   screens: {
-//     Home: HomePageScreen,
-//     // Profile: UserPageScreen,
-//     // JoinGroup: JoinGroupScreen,
-//   },
-// });
 
 function HomePageScreen() {
-  return <SafeAreaView>{/* <MyTabs /> */}</SafeAreaView>;
+  return <SafeAreaView></SafeAreaView>;
 }
 
 export default HomePageScreen;
