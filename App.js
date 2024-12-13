5a0c2f80dadbd706469bcf428c5e35ae88824da9
Jonathan Tromp <jonatromp45@gmail.com>
2024-12-13 16:08:41 +0000

    deleted unused import statements

--- App.js
+++ App.js
@@ -1,19 +1,4 @@
-import LoginScreen from "./app/screens/LoginScreen";
-import CreateAccountScreen from "./app/screens/CreateAccountScreen";
-import HomePageScreen from "./app/screens/HomePageScreen";
-import UserPageScreen from "./app/screens/UserPageScreen";
-import JoinGroupScreen from "./app/screens/JoinGroupScreen";
 import MainContainer from "./app/screens/MainContainer";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, PlatformPressable } from "react-native";
-import {
-  NavigationContainer,
-  useLinkBuilder,
-  useTheme,
-} from "@react-navigation/native";
-// import { PlatformPressable } from "react-native/elements";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 export default function App() {
   return <MainContainer />;
--- app/screens/HomePageScreen.js
+++ app/screens/HomePageScreen.js
@@ -1,8 +1,5 @@
 import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Text, SafeAreaView } from "react-native";
-
-const Tab = createBottomTabNavigator();
 
 function HomePageScreen({ navigation }) {
   return (
