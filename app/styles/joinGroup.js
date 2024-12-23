ecd845c3c764072e00e437f04202a3efc7c510ed
Kyle-Wearing <kylewearing@icloud.com>
2024-12-23 16:33:50 +0000

    modal progress

---
+++ app/styles/joinGroup.js
@@ -0,0 +1,40 @@
+import { StyleSheet, Dimensions } from "react-native";
+const { width, height } = Dimensions.get("window");
+
+export const joinGroup = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    height: 50,
+    width: "100%",
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: "#ccc",
+    padding: 15,
+    fontSize: 16,
+    backgroundColor: "#fff",
+    flexDirection: "row",
+  },
+  modalView: {
+    width: width * 0.5,
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 25,
+    padding: 25,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    borderWidth: 1,
+    borderRadius: 15,
+    borderColor: "",
+  },
+});
