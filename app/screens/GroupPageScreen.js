84582b531dfbde5ccc7c02f74014af7fcc14271a
Kyle-Wearing <kylewearing@icloud.com>
2024-12-24 13:14:24 +0000

    can create and join groups and members are stored in the group

--- app/screens/GroupPageScreen.js
+++ app/screens/GroupPageScreen.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
-import { SafeAreaView, ScrollView, Text } from "react-native";
+import { SafeAreaView, Text, TextInput } from "react-native";
 import { getGroupByGroupId } from "../../until";
 import { groupPage } from "../styles/groupPage";
 import { View } from "react-native";
+import { Button } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export function GroupsPageScreen({ route }) {
   const { id } = route.params;
+  const navigation = useNavigation();
 
   const [name, setName] = useState("");
   const [members, setMembers] = useState([]);
@@ -13,8 +16,15 @@ export function GroupsPageScreen({ route }) {
   useEffect(() => {
     const getGroupData = async () => {
       const group = await getGroupByGroupId(id);
-      setMembers(group.members);
+      const memberArr = [];
+      for (const member in group.members) {
+        memberArr.push({
+          uid: member,
+          username: group.members[member].username,
+        });
+      }
       setName(group.groupName);
+      setMembers(memberArr);
     };
 
     getGroupData();
@@ -23,11 +33,19 @@ export function GroupsPageScreen({ route }) {
   return (
     <SafeAreaView style={groupPage.container}>
       <Text>{name}</Text>
+      <Text>group id</Text>
+      <TextInput value={id}></TextInput>
       <View>
         {members.map((member) => {
           return <Text key={member.uid}>{member.username}</Text>;
         })}
       </View>
+      <Button
+        title="go back"
+        onPress={() => {
+          navigation.goBack();
+        }}
+      ></Button>
     </SafeAreaView>
   );
 }
