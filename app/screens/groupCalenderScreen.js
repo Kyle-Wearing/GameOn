3c73a9000d34a24c79e07f35aab8cab5683dfbbe
Kyle-Wearing <kylewearing@icloud.com>
2024-12-25 14:39:13 +0000

    made a calander screen

---
+++ app/screens/groupCalenderScreen.js
@@ -0,0 +1,6 @@
+import { SafeAreaView, Text } from "react-native";
+import { groupCalander } from "../styles/groupCalander";
+
+export function GroupCalanderScreen() {
+  return <SafeAreaView style={groupCalander.container}></SafeAreaView>;
+}
