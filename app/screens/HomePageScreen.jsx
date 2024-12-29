ee6b60c6dcf118ab58df41204cef77872d7cab7c
Kyle-Wearing <kylewearing@icloud.com>
2024-12-29 01:15:20 +0000

    fix bug with not rendering inital group join/create

--- app/screens/HomePageScreen.jsx
+++ app/screens/HomePageScreen.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { ScrollView, TouchableOpacity } from "react-native";
 import { Text } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { getGroupsByUID } from "../../until";
+import { getGroupsByUID, getUser } from "../../until";
 import { UserContext } from "../../userContext";
 import { useIsFocused, useNavigation } from "@react-navigation/native";
 import { homePage } from "../styles/homePage";
@@ -10,27 +10,39 @@ import { homePage } from "../styles/homePage";
 function HomePageScreen() {
   const { user } = useContext(UserContext);
   const [groups, setGroups] = useState([]);
-  const navigate = useNavigation();
+  const navigation = useNavigation();
 
   const isFocused = useIsFocused();
 
   useEffect(() => {
-    if (user.groups) {
-      getGroupsByUID(user.uid).then((newGroups) => {
-        const groupArr = [];
-        for (const group in newGroups) {
-          groupArr.push({
-            group_id: group,
-            groupName: newGroups[group].groupName,
-          });
-        }
+    getGroupsByUID(user.uid).then((newGroups) => {
+      const groupArr = [];
+      for (const group in newGroups) {
+        groupArr.push({
+          group_id: group,
+          groupName: newGroups[group].groupName,
+        });
+      }
+      if (groupArr.length) {
         setGroups(groupArr);
-      });
-    }
+      } else {
+        setGroups([
+          {
+            group_id: "to join groups",
+            groupName:
+              "you are not in any groups, join or create groups and they will appear here!",
+          },
+        ]);
+      }
+    });
   }, [isFocused]);
 
   function handlePress(id) {
-    navigate.navigate("GroupScreen", { id });
+    if (id === "to join groups") {
+      navigation.navigate("GameOn", { screen: "Groups" });
+    } else {
+      navigation.navigate("GroupScreen", { id });
+    }
   }
 
   return (
