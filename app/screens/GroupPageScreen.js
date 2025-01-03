13338ee9494ac7542ceb2a4ecf6b270b6b7b4c24
Kyle-Wearing <kylewearing@icloud.com>
2025-01-03 13:32:06 +0000

    safe area provider

--- app/screens/GroupPageScreen.js
+++ app/screens/GroupPageScreen.js
@@ -11,6 +11,7 @@ import {
 import { ScrollView } from "react-native";
 import { TouchableOpacity } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export function GroupsPageScreen({ route }) {
   const { id } = route.params;
@@ -43,96 +44,102 @@ export function GroupsPageScreen({ route }) {
   );
 
   return (
-    <SafeAreaView>
-      <View style={groupPage.header}>
-        <TouchableOpacity
-          style={groupPage.backIcon}
-          onPress={() => {
-            navigation.goBack();
-          }}
-        >
-          <Ionicons
-            name="chevron-back-outline"
-            size={30}
-            color="black"
-          ></Ionicons>
-        </TouchableOpacity>
-        <View style={groupPage.title}>
-          <Text style={groupPage.titleText}>{name}</Text>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <View style={groupPage.header}>
+          <TouchableOpacity
+            style={groupPage.backIcon}
+            onPress={() => {
+              navigation.goBack();
+            }}
+          >
+            <Ionicons
+              name="chevron-back-outline"
+              size={30}
+              color="black"
+            ></Ionicons>
+          </TouchableOpacity>
+          <View style={groupPage.title}>
+            <Text style={groupPage.titleText}>{name}</Text>
+          </View>
         </View>
-      </View>
-      <View style={groupPage.container}>
-        <View style={groupPage.leaderboard}>
-          <Text style={groupPage.leaderboardText}>leaderboard</Text>
-          <ScrollView showsVerticalScrollIndicator={false}>
-            {members.map((member, index) => {
-              return (
-                <View
-                  key={member.uid}
-                  style={
-                    index === 0
-                      ? groupPage.memberCard0
-                      : index === 1
-                      ? groupPage.memberCard1
-                      : index === 2
-                      ? groupPage.memberCard2
-                      : groupPage.memberCard
-                  }
-                >
-                  <Text style={groupPage.username}>
-                    {index + 1}: {member.username}
-                  </Text>
-                  <View style={groupPage.statsContainer}>
-                    <Text style={groupPage.score}>score: {member.score}</Text>
-                    <Text style={groupPage.score}>wins: {member.wins}</Text>
+        <View style={groupPage.container}>
+          <View style={groupPage.leaderboard}>
+            <Text style={groupPage.leaderboardText}>leaderboard</Text>
+            <ScrollView showsVerticalScrollIndicator={false}>
+              {members.map((member, index) => {
+                return (
+                  <View
+                    key={member.uid}
+                    style={
+                      index === 0
+                        ? groupPage.memberCard0
+                        : index === 1
+                        ? groupPage.memberCard1
+                        : index === 2
+                        ? groupPage.memberCard2
+                        : groupPage.memberCard
+                    }
+                  >
+                    <Text style={groupPage.username}>
+                      {index + 1}: {member.username}
+                    </Text>
+                    <View style={groupPage.statsContainer}>
+                      <Text style={groupPage.score}>score: {member.score}</Text>
+                      <Text style={groupPage.score}>wins: {member.wins}</Text>
+                    </View>
                   </View>
-                </View>
-              );
-            })}
-          </ScrollView>
+                );
+              })}
+            </ScrollView>
+          </View>
         </View>
-      </View>
-      <View style={groupPage.buttonContainer}>
-        <View style={groupPage.messages}>
+        <View style={groupPage.buttonContainer}>
+          <View style={groupPage.messages}>
+            <TouchableOpacity
+              style={groupPage.button}
+              onPress={() => alert("to be added")}
+            >
+              <Text style={groupPage.buttonText}>messages</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={groupPage.settings}>
+            <TouchableOpacity
+              style={groupPage.button}
+              onPress={() => {
+                navigation.navigate("GroupSettingsScreen", {
+                  groupName: name,
+                  groupMembers: members,
+                  group_id: id,
+                });
+              }}
+            >
+              <Text style={groupPage.buttonText}>settings</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={groupPage.calander}>
+            <TouchableOpacity
+              style={groupPage.button}
+              onPress={() =>
+                navigation.navigate("GroupCalanderScreen", {
+                  id,
+                  members,
+                  name,
+                })
+              }
+            >
+              <Text style={groupPage.buttonText}>calander</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={groupPage.history}></View>
           <TouchableOpacity
             style={groupPage.button}
             onPress={() => alert("to be added")}
           >
-            <Text style={groupPage.buttonText}>messages</Text>
+            <Text style={groupPage.buttonText}>game history</Text>
           </TouchableOpacity>
         </View>
-        <View style={groupPage.settings}>
-          <TouchableOpacity
-            style={groupPage.button}
-            onPress={() => {
-              navigation.navigate("GroupSettingsScreen", {
-                groupName: name,
-                groupMembers: members,
-                group_id: id,
-              });
-            }}
-          >
-            <Text style={groupPage.buttonText}>settings</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={groupPage.calander}>
-          <TouchableOpacity
-            style={groupPage.button}
-            onPress={() =>
-              navigation.navigate("GroupCalanderScreen", { id, members, name })
-            }
-          >
-            <Text style={groupPage.buttonText}>calander</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={groupPage.history}></View>
-        <TouchableOpacity
-          style={groupPage.button}
-          onPress={() => alert("to be added")}
-        >
-          <Text style={groupPage.buttonText}>game history</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
